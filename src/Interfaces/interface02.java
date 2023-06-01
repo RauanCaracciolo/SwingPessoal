31955903741d36a324502a5ce11dcefad39038a5
Rauan Caracciolo <rauancaracciolo@gmail.com>
2023-06-01 18:05:12 -0300

    Atualização da interface propria

--- src/Interfaces/interface02.java
+++ src/Interfaces/interface02.java
@@ -12,38 +12,60 @@ public class interface02 extends JFrame implements ActionListener{
     private JButton botaoEnviar, botaoSair;
     public interface02(){
         setTitle("Interface02");
-        setSize(500,500);
+        setSize(600,400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
-
-        labelVel = new JLabel("Velocidade: ");
-        areaVelocidade = new JTextArea(1,20);
-        labelPosicaoI = new JLabel("Posição Inicial: ");
-        areaPosicaoi = new JTextArea(1, 20);
-        labelPosicaoF = new JLabel("Posição Final: ");
-        areaPosicaof = new JTextArea(1, 20);
-        labelInstantI = new JLabel("Instante Inicial: ");
-        areaInstantI = new JTextArea(1,20);
-        labelInstantf = new JLabel("Instante Final: ");
-        areaInstantf = new JTextArea(1,20);
-        botaoEnviar = new JButton("Enviar");
-        botaoSair = new JButton("Sair");
-        add(labelVel);
-        add(areaVelocidade);
-        add(labelPosicaoI);
-        add(areaPosicaoi);
-        add(labelPosicaoF);
-        add(areaPosicaof);
-        add(labelInstantI);
-        add(areaInstantI);
-        add(labelInstantf);
-        add(areaInstantf);
-        add(botaoEnviar);
-        add(botaoSair);
+        setLayout(null);
         setVisible(true);
 
-        botaoEnviar.getActionListeners();
+        labelVel = new JLabel("Velocidade: ");
+        labelVel.setBounds(120,0,80, 30);
+        add(labelVel);
+
+        areaVelocidade = new JTextArea(1,20);
+        areaVelocidade.setBounds(200, 5, 250, 20);
+        add(areaVelocidade);
+
+        labelPosicaoI = new JLabel("Posição Ini: ");
+        labelPosicaoI.setBounds(120, 40, 80, 30);
+        add(labelPosicaoI);
+
+        areaPosicaoi = new JTextArea(1, 20);
+        areaPosicaoi.setBounds(200, 45, 250, 20);
+        add(areaPosicaoi);
+
+        labelPosicaoF = new JLabel("Posição Fim: ");
+        labelPosicaoF.setBounds(120, 80, 80, 30);
+        add(labelPosicaoF);
+
+        areaPosicaof = new JTextArea(1, 20);
+        areaPosicaof.setBounds(200, 85, 250, 20);
+        add(areaPosicaof);
+
+        labelInstantI = new JLabel("Instante Inicial: ");
+        labelInstantI.setBounds(120, 120, 80, 30);
+        add(labelInstantI);
+
+        areaInstantI = new JTextArea(1,20);
+        areaInstantI.setBounds(200, 125, 250, 20);
+        add(areaInstantI);
+
+        labelInstantf = new JLabel("Instante Final: ");
+        labelInstantf.setBounds(120, 160, 80, 30);
+        add(labelInstantf);
+
+        areaInstantf = new JTextArea(1,20);
+        areaInstantf.setBounds(200, 165, 250, 20);
+        add(areaInstantf);
+
+        botaoEnviar = new JButton("Enviar");
+        botaoEnviar.setBounds(300,200,80, 30);
+        add(botaoEnviar);
+
+        botaoSair = new JButton("Sair");
+        botaoSair.setBounds(200,200,80,30);
+        add(botaoSair);
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         //new interface00();
         //interface01.mostraInterface01();
-        new interface01();
+        new interface02();
     }
 }
