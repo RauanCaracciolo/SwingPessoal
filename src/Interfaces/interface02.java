2519c3181717533f8e51255e426f725993a1121b
Rauan Caracciolo <rauancaracciolo@gmail.com>
2023-06-01 20:07:56 -0300

    Atualização da interface propria

--- src/Interfaces/interface02.java
+++ src/Interfaces/interface02.java
@@ -4,26 +4,24 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class interface02 extends JFrame implements ActionListener{
-    private JFrame meuFrame;
-    private JPanel meupainel;
+public class interface02 extends JFrame implements ActionListener {
     private JTextArea areaVelocidade, areaPosicaoi, areaPosicaof, areaInstantI, areaInstantf;
     private JLabel labelVel, labelPosicaoI, labelPosicaoF, labelInstantI, labelInstantf;
     private JButton botaoEnviar, botaoSair;
-    public interface02(){
+
+    public interface02() {
         setTitle("Interface02");
-        setSize(600,400);
+        setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);
         setLayout(null);
-        setVisible(true);
 
         labelVel = new JLabel("Velocidade: ");
-        labelVel.setBounds(120,0,80, 30);
+        labelVel.setBounds(120, 0, 80, 30);
         add(labelVel);
 
-        areaVelocidade = new JTextArea(1,20);
+        areaVelocidade = new JTextArea(1, 20);
         areaVelocidade.setBounds(200, 5, 250, 20);
         add(areaVelocidade);
 
@@ -43,33 +41,42 @@ public class interface02 extends JFrame implements ActionListener{
         areaPosicaof.setBounds(200, 85, 250, 20);
         add(areaPosicaof);
 
-        labelInstantI = new JLabel("Instante Inicial: ");
+        labelInstantI = new JLabel("Instante Ini: ");
         labelInstantI.setBounds(120, 120, 80, 30);
         add(labelInstantI);
 
-        areaInstantI = new JTextArea(1,20);
+        areaInstantI = new JTextArea(1, 20);
         areaInstantI.setBounds(200, 125, 250, 20);
         add(areaInstantI);
 
-        labelInstantf = new JLabel("Instante Final: ");
+        labelInstantf = new JLabel("Instante Fim: ");
         labelInstantf.setBounds(120, 160, 80, 30);
         add(labelInstantf);
 
-        areaInstantf = new JTextArea(1,20);
+        areaInstantf = new JTextArea(1, 20);
         areaInstantf.setBounds(200, 165, 250, 20);
         add(areaInstantf);
 
         botaoEnviar = new JButton("Enviar");
-        botaoEnviar.setBounds(300,200,80, 30);
+        botaoEnviar.setBounds(300, 200, 80, 30);
         add(botaoEnviar);
 
         botaoSair = new JButton("Sair");
-        botaoSair.setBounds(200,200,80,30);
+        botaoSair.setBounds(200, 200, 80, 30);
         add(botaoSair);
+        setVisible(true);
+        botaoEnviar.addActionListener(this::formulaTeste);
     }
+
+    private void formulaTeste(ActionEvent actionEvent) {
+        double velocidade = Double.parseDouble(areaVelocidade.getText());
+        System.out.println(velocidade);
+    }
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
         JOptionPane.showMessageDialog(null, "Enviado");
+
     }
 }
